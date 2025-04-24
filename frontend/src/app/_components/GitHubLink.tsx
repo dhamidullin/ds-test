d28165214f8c7f7d40463e4fb0abfd64c8c0d319
Danis Khamidullin <danis.zp.123@gmail.com>
2025-04-24 16:25:22 +0300

    Better code style for home page related components

--- frontend/src/app/_components/GitHubLink.tsx
+++ frontend/src/app/_components/GitHubLink.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { GitHubIcon } from '@/components/icons/GitHubIcon';
+import GitHubIcon from '@/components/icons/GitHubIcon';
 
-// Component for the GitHub link
-export const GitHubLink: React.FC = () => {
-  return (
-    <div>
-      <a
-        href="https://github.com/dhamidullin/ds-test"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-white hover:text-gray-200 hover:underline flex items-center justify-center gap-2"
-      >
-        <GitHubIcon />
-        View on GitHub
-      </a>
-    </div>
-  );
-}; 
+const GitHubLink = () => (
+  <div>
+    <a
+      href="https://github.com/dhamidullin/ds-test"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-white hover:text-gray-200 hover:underline flex items-center justify-center gap-2"
+    >
+      <GitHubIcon />
+      View on GitHub
+    </a>
+  </div>
+);
+
+export default GitHubLink; 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,16 @@
 import { Metadata } from 'next'
-import { LandingBackground } from '@/app/_components/LandingBackground'
-import { LandingHeroCard } from '@/app/_components/LandingHeroCard'
+import LandingBackground from '@/app/_components/LandingBackground'
+import LandingHeroCard from '@/app/_components/LandingHeroCard'
 
 export const metadata: Metadata = {
   title: 'Home',
 }
 
-export default function Home() {
-  return (
-    <div className="min-h-screen relative">
-      <LandingBackground />
-      <LandingHeroCard />
-    </div>
-  )
-}
+const Home = () => (
+  <div className="min-h-screen relative">
+    <LandingBackground />
+    <LandingHeroCard />
+  </div>
+);
+
+export default Home;
