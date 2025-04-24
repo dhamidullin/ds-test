eaa1aa4ee1c74b17c6006cddae1c64bcc11aed9f
Danis Khamidullin <danis.zp.123@gmail.com>
2025-04-24 15:29:00 +0300

    - Added tailwind config - moved theme from layout to tailwind config

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "sonner";
 import "./globals.css";
 import { FC } from "react";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
   title: "Task Manager",
@@ -22,7 +11,7 @@ export const metadata: Metadata = {
 const RootLayout: FC<Readonly<{ children: React.ReactNode }>> = ({ children }) => {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="font-sans antialiased">
         {children}
 
         <Toaster richColors position="top-right" />
